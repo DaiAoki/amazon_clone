80cf545c341cf34345202eb24b18432f42ac914c
DaiAoki <a.dai.0814ap@gmail.com>
2020-09-21 15:32:12 +0900

    add include option to Serializer

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::ProductsController < Api::ApplicationController
   def index
     products = Product.all
-    json_string = ProductSerializer.new(products).serialized_json
+    json_string = ProductSerializer.new(products, include: [:category]).serialized_json
 
     render json: json_string, status: :ok
   end
 
   def show
     product = Product.find(params[:id])
-    json_string = ProductSerializer.new(product).serialized_json
+    json_string = ProductSerializer.new(product, include: [:category]).serialized_json
 
     render json: json_string, status: :ok
   end
